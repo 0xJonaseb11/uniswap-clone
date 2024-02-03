5e488d578926fcf9fd5d4024222e2b62708d8c7a
Jaz-3-0 <sebejaz99@gmail.com>
2024-02-03 04:37:53 +0200

    init update

--- client/uniswap-clone-infura/uniswap-app/utils/SupportedCoins.js
+++
@@ -1,5 +0,0 @@
-export const ETH = 'Eth'
-export const COINA = 'CoinA'
-export const COINB = 'CoinB'
-export const COINC = 'CoinC'
-export const DEFAULT_VALUE = 'Select a token'
--- client/uniswap-clone-infura/uniswap-contracts/migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-const CustomDex = artifacts.require("CustomDex.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(CustomDex);
-};
